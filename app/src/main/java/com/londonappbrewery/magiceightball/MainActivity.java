9f8e6810bb287c9cbf11f140b3ec3db4367a3fb3
cosmicplonker <gooner45011@gmail.com>
2018-03-05 22:06:49 +0000

    Images added to the app

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askMe;
+        askMe = findViewById(R.id.askButton);
+
+
+
+
+        final int [] eb_array =  {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+        };
+
+
+
+        askMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rng = new Random();
+                ImageView eightBall = findViewById(R.id.eightBallImg);
+                eightBall.setImageResource(eb_array[rng.nextInt(5)]);
+            }
+        });
+
     }
 }
